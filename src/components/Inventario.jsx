e443a72247d0e7103b2f1a1f2d6270ad9863bb39
Jose Romero <parrajose27@hotmail.com>
2026-04-13 15:46:59 -0500

    agregando funcion de agragar productos

--- src/components/Inventario.jsx
+++ src/components/Inventario.jsx
@@ -5,6 +5,50 @@ import { useEffect, useState } from "react";
 function Inventario() {
   const [productos, setProductos] = useState([]);
 
+  const [form, setForm] = useState({
+  nombre: "",
+  categoria: "",
+  precio: "",
+  cantidad: "",
+  ubicacion: "",
+  stockMinimo: ""
+});
+
+const handleChange = (e) => {
+  setForm({
+    ...form,
+    [e.target.name]: e.target.value
+  });
+};
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+
+  fetch("http://localhost:3000/api/inventario", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      ...form,
+      precio: Number(form.precio),
+      cantidad: Number(form.cantidad),
+      stockMinimo: Number(form.stockMinimo)
+    })
+  })
+    .then((res) => res.json())
+    .then(() => {
+      alert("Producto agregado ✅");
+
+      return fetch("http://localhost:3000/api/inventario");
+    })
+    .then((res) => res.json())
+    .then((data) => setProductos(data))
+    .catch((error) => console.error("Error:", error));
+};
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
   useEffect(() => {
     fetch("http://localhost:3000/api/inventario")
       .then((res) => res.json())
@@ -20,6 +64,18 @@ function Inventario() {
   return (
     <div>
       <h2>Inventario</h2>
+      <h3>Agregar Producto</h3>
+
+<form onSubmit={handleSubmit}>
+  <input name="nombre" placeholder="Nombre" onChange={handleChange} />
+  <input name="categoria" placeholder="Categoría" onChange={handleChange} />
+  <input name="precio" placeholder="Precio" onChange={handleChange} />
+  <input name="cantidad" placeholder="Cantidad" onChange={handleChange} />
+  <input name="ubicacion" placeholder="Ubicación" onChange={handleChange} />
+  <input name="stockMinimo" placeholder="Stock Mínimo" onChange={handleChange} />
+
+  <button type="submit">Agregar</button>
+</form>
 
       <table border="1">
         <thead>
